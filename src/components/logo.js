20744c3501f582be25c2bfdb29d91f410e7e0a7e
mounerus <mounerus@gmail.com>
2022-01-26 21:28:11 +0100

    logo img

--- src/components/logo.js
+++ src/components/logo.js
@@ -3,7 +3,10 @@ import { Link } from "gatsby"
 
 const Logo = props => (
   <div className="site-logo">
-    <Link to="/">{props.title}</Link>
+    {/* <Link to="/">{props.title}</Link> */}
+    <Link to="/">
+      <img src="/static/assets/logol.svg" alt="logo" />
+    </Link>
   </div>
 )
 
